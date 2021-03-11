5f4cc8de0ee6e8dc9a372c6c4ebb529138f27287
Mario Sanchez <mgsanchezdev@gmail.com>
2021-03-11 16:20:31 -0300

    Desafio clase 3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './image/logo.png';
-import './App.css';
+import Home from './components/Home/Home';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <a
-          className="App-link"
-          href="https://www.coderhouse.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          CoderHouse
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <Home />
+    </>
   );
 }
 
